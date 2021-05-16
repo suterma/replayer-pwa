0dae46956489def89b0da98bd68f598b25f94aa3
Marcel <git@marcelsuter.ch>
2021-05-16 13:23:52 +0200

    Added development page

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,15 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/development",
+    name: "Development",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Development.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/Development.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="development">
+        <img alt="Vue logo" src="../assets/logo.png" />
+<p>
+        //TODO's
+        <ul>
+            <li>LOAD a REZ file and show the content/file names                
+            </li>
+            <li>Play the first mp3 file automatically                
+            </li>
+            <li>Parse the XML compilation and show the content
+            </li>
+            <li>Select a UI framework
+            </li>
+        </ul>
+</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    components: {},
+})
+export default class Development extends Vue {}
+</script>
