2e535d2840ef0aab80af5bc014345abdfd191db0
marcel <github@marcelsuter.ch>
2021-08-15 22:47:13 +0200

    Added progress indication

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome the Replayer PWA (Vue.js + TypeScript) App" />
+        <HelloWorld
+            msg="Welcome to the Replayer PWA (Vue.js + TypeScript) App"
+        />
     </div>
 </template>
 
