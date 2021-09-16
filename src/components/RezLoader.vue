431654b7c7b9eb3779ea77fc4d42f03591aa2520
Marcel Suter <github@marcelsuter.ch>
2021-09-16 21:04:18 +0000

    nicer input file button

--- src/components/RezLoader.vue
+++ src/components/RezLoader.vue
@@ -1,15 +1,28 @@
 <template>
     <div>
-        <p>
-            <!-- REZ files are ZIP-Files, with just the ending being .REZ -->
-            <input
-                type="file"
-                id="file-input"
-                accept=".rex,.rez,.zip,.mp3,PlayList"
-                multiple
-                @change="loadFile"
-            />
-        </p>
+        <div class="file is-primary is-large">
+            <label class="file-label">
+                <input
+                    class="file-input"
+                    type="file"
+                    id="file-input"
+                    accept=".rex,.rez,.zip,.mp3,PlayList"
+                    multiple
+                    @change="loadFile"
+                    name="resume"
+                />
+                <span class="file-cta">
+                    <span class="file-icon">
+                        <i class="fas fa-upload"></i>
+                    </span>
+                    <span class="file-label"> Choose a file… </span>
+                </span>
+                <!-- <span class="file-name">
+                    {{selectedFile}}}
+                </span> -->
+            </label>
+        </div>
+
         <p>This component loads Compilatons from a local file.</p>
     </div>
 </template>
