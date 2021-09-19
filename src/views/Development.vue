fa3cbdb428cef74ffcbb312ea4e60375dbd52dd6
marcel <github@marcelsuter.ch>
2021-09-19 22:06:28 +0200

    Creating XML first draft

--- src/views/Development.vue
+++ src/views/Development.vue
@@ -1,56 +1,27 @@
 <template>
-    <div class="development">
-        <h1>1) Select a REZ or ZIP file</h1>
-        <RezLoader />
+    <h1>1) Select a REZ or ZIP file</h1>
+    <RezLoader />
 
-        <!-- //TODO later only show when something is loaded -->
-        <template v-if="true">
-            <h1>2) See and play the compilation</h1>
-            <CompilationDisplay :compilation="compilation" />
-        </template>
-
-        <hr />
-        //TODO's
-        <ul>
-            <li>Make the track players responsive</li>
-            <li>
-                Stop playing when another player starts playing (by default).
-                For this propagate the playing state to the global store. Then,
-                create a watch that watches for events on OTHER players.
-            </li>
-            <li>
-                Make a "tracking" option, that allows to play more than one
-                track simultaneously, in synch
-            </li>
-            <li>Make the players "full width", when showing the cues</li>
-            <li>
-                Create a "live" view, with just one track in full screen mode
-            </li>
-        </ul>
-    </div>
+    <template v-if="compilation">
+        <h1>2) See the compilation as XML</h1>
+        <CompilationXmlDisplay :compilation="compilation" />
+    </template>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import RezLoader from '@/components/RezLoader.vue'; // @ is an alias to /src
-import { ICompilation, ITrack } from '@/store/compilation-types';
-import CompilationDisplay from '@/components/CompilationDisplay.vue';
+import { ICompilation } from '@/store/compilation-types';
+import CompilationXmlDisplay from '@/components/CompilationXmlDisplay.vue';
 
 export default defineComponent({
     components: {
         RezLoader,
-        CompilationDisplay,
+        CompilationXmlDisplay,
     },
     computed: {
         compilation(): ICompilation {
             return this.$store.getters.compilation;
-        },
-        tracks(): Array<string> {
-            return (this.$store.getters.compilation as ICompilation).Tracks.map(
-                function (item: ITrack) {
-                    return item.Name;
-                },
-            );
         },
     },
 });
