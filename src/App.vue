6485c855cf031f328530aa95103fcd5c67be117c
Marcel Suter <github@marcelsuter.ch>
2021-09-26 17:33:05 +0000

    Progress display in separate component

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,20 @@
     <section class="section">
         <div id="content" class="content">
             <router-view />
-            <div :class="{ modal: true, 'is-active': hasProgressMessage }">
-                <div class="modal-background"></div>
-                <div class="modal-content">
-                    <progress class="progress" max="100"></progress>
-
-                    <div class="has-text-centered">
-                        {{ progressMessage }}
-                    </div>
-                </div>
-                <!-- <button
-                    class="modal-close is-large"
-                    aria-label="close"
-                ></button> -->
-            </div>
         </div>
     </section>
-
+    <ProgressOverlay />
     <NavbarBottom />
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import NavbarTop from '@/components/NavbarTop.vue';
 import NavbarBottom from '@/components/NavbarBottom.vue';
+import ProgressOverlay from '@/components/ProgressOverlay.vue';
 
 export default defineComponent({
     name: 'App',
-    components: { NavbarTop, NavbarBottom },
+    components: { NavbarTop, NavbarBottom, ProgressOverlay },
     computed: {
         progressMessage(): string {
             return this.$store.getters.progressMessage;
---
+++ src/components/ProgressOverlay.vue
@@ -0,0 +1,34 @@
+<template>
+    <div :class="{ modal: true, 'is-active': hasProgressMessage }">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <progress class="progress" max="100"></progress>
+
+            <div class="has-text-centered">
+                {{ progressMessage }}
+            </div>
+        </div>
+        <!-- <button
+                    class="modal-close is-large"
+                    aria-label="close"
+                ></button> -->
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {},
+    computed: {
+        progressMessage(): string {
+            return this.$store.getters.progressMessage;
+        },
+        hasProgressMessage(): boolean {
+            return (
+                this.progressMessage != null && this.progressMessage.length > 0
+            );
+        },
+    },
+});
+</script>
