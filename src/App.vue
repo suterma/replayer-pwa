882dd58a3eac8569ae2c8f3a5219fe0118531290
marcel <github@marcelsuter.ch>
2021-10-10 23:49:58 +0200

    Progress into custom modal

--- src/App.vue
+++ src/App.vue
@@ -4,44 +4,20 @@
     <!-- The page content -->
     <section class="section">
         <div id="content" class="content">
-            {{ progressMessage }}
             <router-view />
-            <div :class="{ modal: true, 'is-active': hasProgressMessage }">
-                <div class="modal-background"></div>
-                <div class="modal-content">
-                    <progress class="progress" max="100"></progress>
-
-                    <div class="has-text-centered">
-                        {{ progressMessage }}
-                    </div>
-                </div>
-                <!-- <button
-                    class="modal-close is-large"
-                    aria-label="close"
-                ></button> -->
-            </div>
         </div>
     </section>
 
-    <NavbarBottom />
+    <!-- The progress modal -->
+    <ProgressModal />
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import NavbarTop from '@/components/NavbarTop.vue';
-import NavbarBottom from '@/components/NavbarBottom.vue';
+import ProgressModal from '@/components/ProgressModal.vue';
 
 export default defineComponent({
     name: 'App',
-    components: { NavbarTop, NavbarBottom },
-    computed: {
-        progressMessage(): string {
-            return this.$store.getters.progressMessage;
-        },
-        hasProgressMessage(): boolean {
-            return (
-                this.progressMessage != null && this.progressMessage.length > 0
-            );
-        },
-    },
+    components: { NavbarTop, ProgressModal },
 });
 </script>
