62ea700ac62a917f51c1ef0b3f5100f099563895
Marcel Suter <github@marcelsuter.ch>
2021-11-10 15:14:53 +0000

    added jest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    "jest/globals": true
   },
   plugins: [
    '@typescript-eslint',
@@ -14,6 +15,7 @@ module.exports = {
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
      'plugin:jest/recommended',
+     "plugin:jest/style",
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -22,5 +24,15 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
+  },
+  settings: {
+    jest: {
+      version: require('jest/package.json').version,
+    },
   },
 };
