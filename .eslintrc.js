3c672ae0d75fc36f43a9303055e9bd74689e1255
Marcel Suter <github@marcelsuter.ch>
2021-11-10 15:37:00 +0000

    formatted

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,35 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    "jest/globals": true
-  },
-  plugins: [
-   '@typescript-eslint',
-   'jest', 
-  ],
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-     'plugin:jest/recommended',
-     "plugin:jest/style",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "jest/no-disabled-tests": "warn",
-    "jest/no-focused-tests": "error",
-    "jest/no-identical-title": "error",
-    "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error"
-  },
-  settings: {
-    jest: {
-      version: require('jest/package.json').version,
+    root: true,
+    env: {
+        node: true,
+        'jest/globals': true,
     },
-  },
+    plugins: ['@typescript-eslint', 'jest'],
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint',
+        'plugin:jest/recommended',
+        'plugin:jest/style',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+    },
+    settings: {
+        jest: {
+            version: require('jest/package.json').version,
+        },
+    },
 };
