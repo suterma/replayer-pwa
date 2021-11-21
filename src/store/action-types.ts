b5d8e80bf4e742d64d295f08980121e792d6bff6
marcel <github@marcelsuter.ch>
2021-11-21 17:27:19 +0100

    storing blobs in indexdb

--- src/store/action-types.ts
+++ src/store/action-types.ts
@@ -1,7 +1,8 @@
 export enum ActionTypes {
     /** //TODO just an example */
-    PLAY_TRACK = 'PLAY_TRACK',
-    /** Retrieves the last compilation from the persistent storage, and commits it into the store as the current compilation*/
+    //PLAY_TRACK = 'PLAY_TRACK',
+    /** Retrieves the last stored compilation from the persistent storage, including all blobs and the selected track,
+     * and commits it all into the store as the current compilation*/
     RETRIEVE_COMPILATION = 'RETRIEVE_COMPILATION',
     //   SET_COMPILATION = 'SET_COMPILATION',
 
@@ -9,4 +10,6 @@ export enum ActionTypes {
     SET_COMPILATION_FROM_XML = 'SET_COMPILATION_FROM_XML',
     /** Parses and uses the given compilation as the new current compilation, while persistently storing it for later retrieval */
     SET_COMPILATION_FROM_PLIST = 'SET_COMPILATION_FROM_PLIST',
+    /** Adds a blob as a media source, using a name and a blob  */
+    ADD_MEDIA_BLOB = 'ADD_MEDIA_BLOB',
 }
