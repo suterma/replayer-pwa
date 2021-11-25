b30f9052540a9e94f311cf8ba6f7ac9ff830d7f5
Marcel Suter <github@marcelsuter.ch>
2021-11-25 22:41:59 +0000

    development with visual player

--- src/views/Development.vue
+++ src/views/Development.vue
@@ -1,6 +1,6 @@
 <template>
     <h1 class="title has-text-danger">Upcoming features for evaluation</h1>
-    <h2 class="subtitle">XML export</h2>
+    <h2 class="subtitle has-text-danger">XML export</h2>
 
     <template v-if="hasCompilation">
         <h2 class="subtitle">See the compilation as XML</h2>
@@ -10,8 +10,10 @@
         <p>Please load a compilation first</p>
     </template>
     <hr />
-    <h1 class="title">Tests</h1>
-    <h2 class="subtitle">Loader component with various compilations</h2>
+    <h1 class="title has-text-danger">Tests</h1>
+    <h2 class="subtitle has-text-danger">
+        Loader component with various compilations
+    </h2>
     <p>
         Try one of the various compilations to check out the various features:
     </p>
@@ -31,17 +33,30 @@
             A bplist file with 21 tracks (no media files).
         </li>
     </ul>
+    <h2 class="subtitle has-text-danger">Track with Visualization</h2>
+    <p>This SHOULD work with 2 visualizations</p>
+    <p>
+        <TrackAudioMeterPlayer
+            ref="player"
+            :title="trackFileUrl?.fileName"
+            src="your-light-by-lidija-roos.mp3"
+            v-on:timeupdate="updateTime"
+            v-on:trackLoaded="calculateCueDurations"
+        ></TrackAudioMeterPlayer>
+    </p>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { ICompilation } from '@/store/compilation-types';
 import CompilationXmlDisplay from '@/components/CompilationXmlDisplay.vue';
+import TrackAudioMeterPlayer from '@/components/TrackAudioMeterPlayer.vue';
 
 export default defineComponent({
     name: 'Development',
     components: {
         CompilationXmlDisplay,
+        TrackAudioMeterPlayer,
     },
     computed: {
         compilation(): ICompilation {
