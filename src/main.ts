da67afaaee871d5313006ec748bfe859865dc918
Marcel Suter <github@marcelsuter.ch>
2021-11-25 22:34:33 +0000

    Added experimental visualization

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import './registerServiceWorker';
 import router from './router';
 import { store } from './store/store';
 import VueScrollTo from 'vue-scrollto';
+import AudioVisual from 'vue-audio-visual';
 
 import outfront from 'outfront'; // import outfront JS
 outfront(); // Call it on top of project.
 
-createApp(App).use(store).use(router).use(VueScrollTo).mount('#app');
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(VueScrollTo)
+    .use(AudioVisual)
+    .mount('#app');
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,3 +5,5 @@ declare module '@vue/runtime-core' {
         $store: Store;
     }
 }
+
+declare module 'vue-audio-visual';
