69c2538985299939545ef6174db8055c054dbc15
marcel <github@marcelsuter.ch>
2021-12-11 23:20:34 +0100

    updated menu

--- src/components/CompilationDropdownHeader.vue
+++ src/components/CompilationDropdownHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <h1 class="title">
-        <span>
+        <span class="">
             {{ compilation?.Title }}
 
             <span class="is-pulled-right ml-3">
@@ -23,7 +23,7 @@
                 >
                     <div class="dropdown-trigger">
                         <button
-                            class="button"
+                            class="button is-ghost"
                             aria-haspopup="true"
                             aria-controls="dropdown-menu"
                             title="Compilation context menu"
@@ -36,7 +36,7 @@
                                     >
                                         <path
                                             fill="currentColor"
-                                            d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"
+                                            d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z"
                                         />
                                     </svg>
                                 </i>
@@ -50,10 +50,11 @@
                                 class="dropdown-item"
                                 @click="downloadXml"
                             >
-                                Download
+                                Download as
                                 <span class="has-text-weight-bold">XML</span>
-                                (<span class="is-family-monospace">.rex</span>),
-                                without media files
+                                compilation (<span class="is-family-monospace"
+                                    >.rex</span
+                                >), without media files
                             </a>
                             <hr class="dropdown-divider" />
                             <a href="#" @click="close" class="dropdown-item">
