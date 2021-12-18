2e7257847caed1ab7dfcccc7d88ef2e610445b4c
marcel <github@marcelsuter.ch>
2021-12-18 23:52:56 +0100

    improved layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,204 +1,223 @@
 <template>
     <div class="container">
         <h1 class="title">About</h1>
-        <p>
-            <a href="https://replayer.app" target="_blank">Replayer</a> is a
-            free, cue-based media player for rehearsals with playback music. By
-            the click of a button, Replayer starts to play at predefined times
-            in the audio file.
-        </p>
-        <p title="App info" class="has-text-right is-italic">
-            <small
-                >App version: {{ version }} ({{ environment }}),
-                {{ git }}</small
-            >
-        </p>
-
-        <hr />
-
-        <WelcomeText />
-        <h1 class="title">Demo</h1>
-        <ul>
-            <li>
-                <a href="/#/play/demo-compilation-featuring-lidija-roos.rez"
-                    >Demo package with a compilation featuring Lidija Roos</a
-                ><br />
-                A REZ Package containing an REX compilation with 2 tracks and
-                matching mp3 files.
-            </li>
-            <li>
-                <a href="/#/play/Demo%20Adonia%20Junior%2021.bplist"
-                    >Demo Binary Property List compilation (as from the
-                    LivePlayback app)</a
-                ><br />
-                A bplist file with 21 tracks (no media files).
-            </li>
-        </ul>
-        <h1 class="title">Credits</h1>
-        <p>This project uses</p>
-        <ul>
-            <li>
-                <a href="https://vuejs.org/" target="_blank">Vue.js</a>, the
-                progressive JavaScript framework. (Released under the MIT
-                License, Copyright © 2014-2021 Evan You)
-            </li>
-            <li>
-                <a href="https://vuex.vuejs.org/" target="_blank">Vuex</a>, a
-                state management pattern + library for Vue.js applications.
-                (Released under the MIT License, by the vuejs team)
-            </li>
-            <li>
-                <a href="https://router.vuejs.org/" target="_blank"
-                    >Vue Router</a
-                >, a the official router for Vue.js. (Released under the MIT
-                License, by the vuejs team)
-            </li>
-            <li>
-                <a
-                    href="https://github.com/yyx990803/register-service-worker"
-                    target="_blank"
-                    >register-service-worker</a
-                >, a script to simplify service worker registration with hooks
-                for common events. (Released under the MIT License, by Evan You)
-            </li>
-            <li>
-                <a href="https://github.com/zloirock/core-js" target="_blank"
-                    >core-js</a
-                >, a modular standard library for JavaScript. (Released under
-                the MIT License, by Denis Pushkarev)
-            </li>
-            <li>
-                <a href="https://realfavicongenerator.net" target="_blank"
-                    >RealFaviconGenerator </a
-                >Favicon Generator. For real. (free to use)
-            </li>
-            <li>
-                <a href="https://stuk.github.io/jszip/" target="_blank"
-                    >JSZip </a
-                >JSZip is a javascript library for creating, reading and editing
-                .zip files, with a lovely and simple API. (Released under the
-                MIT License, by Stuart Knightley)
-            </li>
-            <li>
-                <a
-                    href="https://github.com/paanSinghCoder/OutFrontJS"
-                    target="_blank"
-                    >OutFrontJS </a
-                >Console for mobile browsers. (Released under the MIT License,
-                by Gaurav Singh)
-            </li>
-            <li>
-                <a
-                    href="https://github.com/Leonidas-from-XIV/node-xml2js"
-                    target="_blank"
-                    >node-xml2js </a
-                >Simple XML to JavaScript object converter. (Released under the
-                MIT License, by Marek Kubica)
-            </li>
-            <li>
-                <a
-                    href="https://vuejsexamples.com/html5-basic-audio-player-with-vue-js/"
-                    target="_blank"
-                    >HTML5 Basic Audio Player with vue.js</a
+        <div class="content">
+            <p>
+                <a href="https://replayer.app" target="_blank">Replayer</a> is a
+                free, cue-based media player for rehearsals with playback music.
+                By the click of a button, Replayer starts to play at predefined
+                times in the audio file.
+            </p>
+            <WelcomeText />
+            <p title="App info" class="has-text-right is-italic">
+                <small
+                    >App version: {{ version }} ({{ environment }}),
+                    {{ git }}</small
                 >
-                Example code (modified)
-            </li>
-            <li>
-                <a href="https://bulma.io/" target="_blank">Bulma</a>, the
-                modern CSS framework that just works. (Released under the MIT
-                License, by Jeremy Thomas)
-            </li>
-            <li>
-                <a href="https://jenil.github.io/bulmaswatch/" target="_blank"
-                    >Bulmaswatch</a
-                >, free themes for Bulma. (Released under the MIT License, by
-                Jenil Gogari)
-            </li>
-            <li>
-                <a
-                    href="https://github.com/joeferner/node-bplist-parser"
-                    target="_blank"
-                    >bplist-parser</a
-                >, a binary Mac OS X Plist (property list) parser. (Released
-                under the MIT License, by Joe Ferner)
-            </li>
-            <li>
-                <a
-                    href="https://github.com/eligrey/FileSaver.js"
-                    target="_blank"
-                    >FileSaver.js</a
-                >, the solution to saving files on the client-side. (Released
-                under the MIT License, by Eli Grey)
-            </li>
-            <li>
-                <a href="https://github.com/uuidjs/uuid" target="_blank">uuid</a
-                >, for the creation of RFC4122 UUIDs. (Released under the MIT
-                License)
-            </li>
-            <li>
-                <a
-                    href="https://github.com/shentao/vue-global-events"
-                    target="_blank"
-                    >vue-global-events</a
-                >, add shortcuts by listening to events on the document,
-                anywhere. (Released under the MIT License, by Damian Dulisz)
-            </li>
-            <li>
-                <a
-                    href="https://github.com/rigor789/vue-scrollto#readme"
-                    target="_blank"
-                    >vue-scrollto</a
-                >, scrolling to elements was never this easy. (Released under
-                the MIT License, by Igor Randjelovic)
-            </li>
-            <li>
-                <a
-                    href="https://github.com/jakearchibald/idb-keyval"
-                    target="_blank"
-                    >IDB-Keyval</a
-                >, a super-simple-small promise-based keyval store implemented
-                with IndexedDB. (Released under the MIT License, by Jake
-                Archibald)
-            </li>
-            <li>
-                <a href="https://materialdesignicons.com/" target="_blank"
-                    >Material Design Icons</a
-                >, the original. Following Google's Material Design guidelines
-                for system icons. (Icons released under the Apache 2.0 License,
-                by pictogrammers.com)
-            </li>
-            <li>
-                <a
-                    href="https://github.com/craigrileyuk/vue3-slide-up-down"
-                    target="_blank"
-                    >vue3-slide-up-down </a
-                >, like jQuery's slideUp / slideDown, but for Vue 3! (Released
-                under the MIT License, by Craig Riley)
-            </li>
-        </ul>
+            </p>
 
+            <hr />
+        </div>
+        <h1 class="title">Demo</h1>
+        <div class="content">
+            <ul>
+                <li>
+                    <a href="/#/play/demo-compilation-featuring-lidija-roos.rez"
+                        >Demo package with a compilation featuring Lidija
+                        Roos</a
+                    ><br />
+                    A REZ Package containing an REX compilation with 2 tracks
+                    and matching mp3 files.
+                </li>
+                <li>
+                    <a href="/#/play/Demo%20Adonia%20Junior%2021.bplist"
+                        >Demo Binary Property List compilation (as from the
+                        LivePlayback app)</a
+                    ><br />
+                    A bplist file with 21 tracks (no media files).
+                </li>
+            </ul>
+        </div>
+        <h1 class="title">Credits</h1>
+        <div class="content">
+            <p>This project uses</p>
+            <ul>
+                <li>
+                    <a href="https://vuejs.org/" target="_blank">Vue.js</a>, the
+                    progressive JavaScript framework. (Released under the MIT
+                    License, Copyright © 2014-2021 Evan You)
+                </li>
+                <li>
+                    <a href="https://vuex.vuejs.org/" target="_blank">Vuex</a>,
+                    a state management pattern + library for Vue.js
+                    applications. (Released under the MIT License, by the vuejs
+                    team)
+                </li>
+                <li>
+                    <a href="https://router.vuejs.org/" target="_blank"
+                        >Vue Router</a
+                    >, a the official router for Vue.js. (Released under the MIT
+                    License, by the vuejs team)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/yyx990803/register-service-worker"
+                        target="_blank"
+                        >register-service-worker</a
+                    >, a script to simplify service worker registration with
+                    hooks for common events. (Released under the MIT License, by
+                    Evan You)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/zloirock/core-js"
+                        target="_blank"
+                        >core-js</a
+                    >, a modular standard library for JavaScript. (Released
+                    under the MIT License, by Denis Pushkarev)
+                </li>
+                <li>
+                    <a href="https://realfavicongenerator.net" target="_blank"
+                        >RealFaviconGenerator </a
+                    >Favicon Generator. For real. (free to use)
+                </li>
+                <li>
+                    <a href="https://stuk.github.io/jszip/" target="_blank"
+                        >JSZip </a
+                    >JSZip is a javascript library for creating, reading and
+                    editing .zip files, with a lovely and simple API. (Released
+                    under the MIT License, by Stuart Knightley)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/paanSinghCoder/OutFrontJS"
+                        target="_blank"
+                        >OutFrontJS </a
+                    >Console for mobile browsers. (Released under the MIT
+                    License, by Gaurav Singh)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/Leonidas-from-XIV/node-xml2js"
+                        target="_blank"
+                        >node-xml2js </a
+                    >Simple XML to JavaScript object converter. (Released under
+                    the MIT License, by Marek Kubica)
+                </li>
+                <li>
+                    <a
+                        href="https://vuejsexamples.com/html5-basic-audio-player-with-vue-js/"
+                        target="_blank"
+                        >HTML5 Basic Audio Player with vue.js</a
+                    >
+                    Example code (modified)
+                </li>
+                <li>
+                    <a href="https://bulma.io/" target="_blank">Bulma</a>, the
+                    modern CSS framework that just works. (Released under the
+                    MIT License, by Jeremy Thomas)
+                </li>
+                <li>
+                    <a
+                        href="https://jenil.github.io/bulmaswatch/"
+                        target="_blank"
+                        >Bulmaswatch</a
+                    >, free themes for Bulma. (Released under the MIT License,
+                    by Jenil Gogari)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/joeferner/node-bplist-parser"
+                        target="_blank"
+                        >bplist-parser</a
+                    >, a binary Mac OS X Plist (property list) parser. (Released
+                    under the MIT License, by Joe Ferner)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/eligrey/FileSaver.js"
+                        target="_blank"
+                        >FileSaver.js</a
+                    >, the solution to saving files on the client-side.
+                    (Released under the MIT License, by Eli Grey)
+                </li>
+                <li>
+                    <a href="https://github.com/uuidjs/uuid" target="_blank"
+                        >uuid</a
+                    >, for the creation of RFC4122 UUIDs. (Released under the
+                    MIT License)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/shentao/vue-global-events"
+                        target="_blank"
+                        >vue-global-events</a
+                    >, add shortcuts by listening to events on the document,
+                    anywhere. (Released under the MIT License, by Damian Dulisz)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/rigor789/vue-scrollto#readme"
+                        target="_blank"
+                        >vue-scrollto</a
+                    >, scrolling to elements was never this easy. (Released
+                    under the MIT License, by Igor Randjelovic)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/jakearchibald/idb-keyval"
+                        target="_blank"
+                        >IDB-Keyval</a
+                    >, a super-simple-small promise-based keyval store
+                    implemented with IndexedDB. (Released under the MIT License,
+                    by Jake Archibald)
+                </li>
+                <li>
+                    <a href="https://materialdesignicons.com/" target="_blank"
+                        >Material Design Icons</a
+                    >, the original. Following Google's Material Design
+                    guidelines for system icons. (Icons released under the
+                    Apache 2.0 License, by pictogrammers.com)
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/craigrileyuk/vue3-slide-up-down"
+                        target="_blank"
+                        >vue3-slide-up-down </a
+                    >, like jQuery's slideUp / slideDown, but for Vue 3!
+                    (Released under the MIT License, by Craig Riley)
+                </li>
+            </ul>
+        </div>
         <h1 class="title">License</h1>
-        <p>
-            Replayer is licensed to you under the
-            <a
-                href="https://github.com/suterma/replayer-pwa/blob/main/LICENSE"
-                target="_blank"
-                >GNU General Public License v3.0</a
-            >
-            or any later version.
-        </p>
-        <p>
-            The source code is availabe on
-            <a href="https://github.com/suterma/replayer-pwa" target="_blank"
-                >GitHub</a
-            >. To report an issue or submit a feature request, use the
-            <a href="https://github.com/suterma/replayer-pwa/issues">issues</a>
-            page on the GitHub repository.
-        </p>
-        <p>
-            Created by
-            <a href="https://marcelsuter.ch" target="_blank">Marcel Suter</a>.
-        </p>
+        <div class="content">
+            <p>
+                Replayer is licensed to you under the
+                <a
+                    href="https://github.com/suterma/replayer-pwa/blob/main/LICENSE"
+                    target="_blank"
+                    >GNU General Public License v3.0</a
+                >
+                or any later version.
+            </p>
+            <p>
+                The source code is availabe on
+                <a
+                    href="https://github.com/suterma/replayer-pwa"
+                    target="_blank"
+                    >GitHub</a
+                >. To report an issue or submit a feature request, use the
+                <a href="https://github.com/suterma/replayer-pwa/issues"
+                    >issues</a
+                >
+                page on the GitHub repository.
+            </p>
+            <p>
+                Created by
+                <a href="https://marcelsuter.ch" target="_blank">Marcel Suter</a
+                >.
+            </p>
+        </div>
     </div>
 </template>
 
--- src/views/Development.vue
+++ src/views/Development.vue
@@ -1,40 +1,49 @@
 <template>
-    <h1 class="title has-text-danger">Upcoming features for evaluation</h1>
-    <h2 class="subtitle has-text-danger">XML export</h2>
-
-    <template v-if="hasCompilation">
-        <h2 class="subtitle">See the compilation as XML</h2>
-        <CompilationXmlDisplay :compilation="compilation" />
-    </template>
-    <template v-else>
-        <p>Please load a compilation first</p>
-    </template>
-    <hr />
+    <section class="hero is-danger is-small">
+        <div class="hero-body">
+            <p class="title">Upcoming features for evaluation</p>
+            <p class="subtitle">Use with caution</p>
+        </div>
+    </section>
+    <h1 class="title has-text-danger">XML export</h1>
+    <div class="content">
+        <template v-if="hasCompilation">
+            <h2 class="subtitle">See the compilation as XML</h2>
+            <CompilationXmlDisplay :compilation="compilation" />
+        </template>
+        <template v-else>
+            <p>Please load a compilation first</p>
+        </template>
+        <hr />
+    </div>
     <h1 class="title has-text-danger">Tests</h1>
+
     <h2 class="subtitle has-text-danger">
         Loader component with various compilations
     </h2>
-    <p>
-        Try one of the various compilations to check out the various features:
-    </p>
-    <ul>
-        <li>
-            <a href="/#/play/demo-compilation-featuring-lidija-roos.rez"
-                >Demo package with a compilation featuring Lidija Roos</a
-            ><br />
-            A REZ Package containing an REX compilation with 2 tracks and
-            matching mp3 files.
-        </li>
-        <li>
-            <a href="/#/play/Demo%20Adonia%20Junior%2021.bplist"
-                >Demo Binary Property List compilation (as from the LivePlayback
-                app)</a
-            ><br />
-            A bplist file with 21 tracks (no media files).
-        </li>
-    </ul>
-    <!-- NOTE: This might have been introducing latency and memory issues. With large compilations, playback was sometimes not possible on a Fairphone 3+. -->
-    <!-- <h2 class="subtitle has-text-danger">Track with Visualization</h2>
+    <div class="content">
+        <p>
+            Try one of the various compilations to check out the various
+            features:
+        </p>
+        <ul>
+            <li>
+                <a href="/#/play/demo-compilation-featuring-lidija-roos.rez"
+                    >Demo package with a compilation featuring Lidija Roos</a
+                ><br />
+                A REZ Package containing an REX compilation with 2 tracks and
+                matching mp3 files.
+            </li>
+            <li>
+                <a href="/#/play/Demo%20Adonia%20Junior%2021.bplist"
+                    >Demo Binary Property List compilation (as from the
+                    LivePlayback app)</a
+                ><br />
+                A bplist file with 21 tracks (no media files).
+            </li>
+        </ul>
+        <!-- NOTE: This might have been introducing latency and memory issues. With large compilations, playback was sometimes not possible on a Fairphone 3+. -->
+        <!-- <h2 class="subtitle has-text-danger">Track with Visualization</h2>
     <p>This SHOULD work with 2 visualizations</p>
     <p>
         <TrackAudioMeterPlayer
@@ -45,7 +54,8 @@
             v-on:trackLoaded="calculateCueDurations"
         ></TrackAudioMeterPlayer>
     </p> -->
-    <h2 class="subtitle has-text-danger">Icons test</h2>
+    </div>
+    <h1 class="title has-text-danger">Icons test</h1>
     <CollapsibleButton
         v-model="isExpanded"
         collapsedText="Show 11 somethings"
