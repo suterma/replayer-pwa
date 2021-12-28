f256a7eb566c78bec9f9814dc7baa04ae4955d86
Marcel Suter <github@marcelsuter.ch>
2021-12-28 23:42:30 +0000

    added fading support

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -35,7 +35,7 @@
         <hr />
 
         <div class="has-text-danger">
-            <h1 class="title has-text-danger">XML export</h1>
+            <h1 class="title has-text-danger">Experimental settings</h1>
             <h3 class="subtitle has-text-danger">
                 Here be dragons (use at your own risk)
             </h3>
@@ -51,6 +51,54 @@
                         Use howler.js as audio engine
                         <span class="has-opacity-half is-size-7">
                             (may behave differently on playback)</span
+                        >
+                    </label>
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label"
+                    >Fading duration
+                    <span class="has-opacity-half is-size-7">
+                        (For play and pause operations)</span
+                    >
+                </label>
+                <div class="control">
+                    <div class="select">
+                        //TODO fix this binding
+                        <!-- <select
+                            v-model.number="this.getSettings.fadingDuration"
+                            @change="fadingDurationChanged"
+                        >
+                            <option v-bind:value="{ number: 0 }">
+                                no fading
+                            </option>
+                            <option v-bind:value="{ number: 16 }">
+                                Fast (16 milliseconds, barely audible)
+                            </option>
+                            <option v-bind:value="{ number: 300 }">
+                                Medium (300 milliseconds)
+                            </option>
+                            <option v-bind:value="{ number: 4000 }">
+                                Slow (4 seconds)
+                            </option>
+                        </select> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <label class="checkbox">
+                        <input
+                            type="checkbox"
+                            :checked="this.getSettings.applyFadeInOffset"
+                            @change="applyFadeInOffsetChanged"
+                        />
+                        Apply an offset before fade-in operations
+                        <span class="has-opacity-half is-size-7">
+                            (The offset compensates for the fade-in
+                            duration)</span
                         >
                     </label>
                 </div>
@@ -92,6 +140,15 @@ export default defineComponent({
             const settings = this.getSettings;
 
             settings.useHowlerJsAudioEngine = checked;
+
+            this.$store.commit(MutationTypes.UPDATE_SETTINGS, settings);
+        },
+
+        applyFadeInOffsetChanged(event: Event) {
+            const checked = (event.target as HTMLInputElement)?.checked;
+            const settings = this.getSettings;
+
+            settings.applyFadeInOffset = checked;
 
             this.$store.commit(MutationTypes.UPDATE_SETTINGS, settings);
         },
