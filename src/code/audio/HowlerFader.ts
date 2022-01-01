f33f8836af3bac9f57f689eb14cbe049412ec99d
marcel <github@marcelsuter.ch>
2022-01-01 14:37:43 +0100

    Add fade-in and fade-out support #56

--- src/code/audio/HowlerFader.ts
+++ src/code/audio/HowlerFader.ts
@@ -25,44 +25,78 @@ export default class HowlerFader {
         this.duration = duration;
         this.applyFadeInOffset = applyFadeInOffset;
 
-        //Initialize by setting the fader level to initially zero
+        //Initialize by setting the fader level to the initial value
         this.cancel();
+    }
+    /** Updates the current settings.
+     * @remarks The settings will be used for the next fade.
+     * However, when the new duration is zero (no fade),
+     * the cancel operation is immediately called, resetting the volume to the initial value for this case.
+     * @param {number} duration - The fading duration. Default is zero (no fade operation called)
+     * @param {boolean} applyFadeInOffset - Whether to apply the seek offset before fade-in operations, to compensate the fading duration. (Default: true)
+     */
+    updateSettings(
+        // eslint-disable-next-line @typescript-eslint/no-inferrable-types
+        duration: number = 0,
+        // eslint-disable-next-line @typescript-eslint/no-inferrable-types
+        applyFadeInOffset: boolean = true,
+    ) {
+        const noMoreFading = this.duration != 0 && duration === 0;
+        this.duration = duration;
+        this.applyFadeInOffset = applyFadeInOffset;
+
+        if (noMoreFading) {
+            //Cancel immediately by setting the fader level to the initial value
+            this.cancel();
+        }
     }
     /** The howler.js howl to act upon */
     sound: Howl;
-    /** The fading duration in [milliseconds] */
+    /** The fading duration in [milliseconds] (zero means no fading)*/
     duration = 0;
     /** Whether to apply a seek offset before fade-in operations, to compensate the fading duration.*/
     applyFadeInOffset = true;
-    /** The minimal audio level (avoid zero due to calculation problems) */
-    minAudioLevel = 0.01;
+    /** The minimum audio level */
+    minAudioLevel = 0;
+    /** The maximum audio level */
+    maxAudioLevel = 1;
 
-    /** Immediately cancel any running fade operation, by immediately fading to the minimum (if the volume is currently non-minimum).
-     * @remarks Cancel is only executed when the duration (of previous fades) is non-zero
+    /** Immediately cancel any running fade operation.
+     * @remarks This immediately fades to the initial value.
+     * When a non-zero fading duration is set, this is the minimum value (finish a possible fade-out/prepare for the next fade-in).
+     * When a zero fading duration is set, this is the maximum value (effectively remove any fade)
      */
     cancel(): void {
         if (this.duration) {
-            console.debug(`HowlerFader::cancel`);
-            //TODO maybe only fade when the level is different???
+            console.debug(`HowlerFader::cancel:toMinimum`);
             const currentVolume = this.getCurrentVolume();
             if (currentVolume != this.minAudioLevel) {
                 this.sound.fade(currentVolume, this.minAudioLevel, 0);
             }
+        } else {
+            console.debug(`HowlerFader::cancel:toMaximum`);
+            const currentVolume = this.getCurrentVolume();
+            if (currentVolume != this.maxAudioLevel) {
+                this.sound.fade(currentVolume, this.maxAudioLevel, 0);
+            }
         }
     }
 
-    /** Gets the current volume, with a boundary check to make sure, it's a valid number between minAudioLevel and 1, inclusive.
-     * If not valid, 0.5 is returned as a compromise. */
+    /** Gets the current volume, with a boundary check to make sure
+     * it's a valid number between minAudioLevel and maxAudioLevel, inclusive.
+     * If not valid, an average level is returned as a compromise. */
     getCurrentVolume(): number {
         let currentVolume = this.sound.volume();
-        console.debug(`HowlerFader::cancel:currentVolume:${currentVolume}`);
+        console.debug(
+            `HowlerFader::getCurrentVolume:currentVolume:${currentVolume}`,
+        );
 
         if (
             currentVolume < this.minAudioLevel ||
-            currentVolume > 1 ||
+            currentVolume > this.maxAudioLevel ||
             isNaN(currentVolume)
         ) {
-            currentVolume = 0.5;
+            currentVolume = (this.minAudioLevel + this.maxAudioLevel) / 2;
         }
         return currentVolume;
     }
@@ -76,10 +110,10 @@ export default class HowlerFader {
     }
 
     /** Returns a linear fade-in promise for the currently playing track
-     * @remarks The sound is faded to full scale.
+     * @remarks The sound is faded to the maximum audio level.
      * An actual fade operation is only started when
      * - the duration is non-zero and
-     * - the current volume is also non-full-scale
+     * - the current volume is also non-maximum
      * otherwise
      * - the promise is immediately resolved.
      * @devdoc Howler only supports linear fade operations
@@ -95,15 +129,16 @@ export default class HowlerFader {
                     if (this.applyFadeInOffset) {
                         this.applyPreFadeOffset();
                     }
-                    if (currentVolume < 1) {
+                    if (currentVolume < this.maxAudioLevel) {
                         //Determine the required fade, based on the current volume
                         //(an existing fade-out could be currently running, requiring a partial fade only)
                         const requiredDuration =
-                            this.duration * (1 - currentVolume);
+                            this.duration *
+                            (this.maxAudioLevel - currentVolume);
 
                         return this.fade(
                             currentVolume,
-                            1,
+                            this.maxAudioLevel,
                             requiredDuration,
                         ).then(() => {
                             //TODO remove this logging for production
@@ -151,10 +186,10 @@ export default class HowlerFader {
     }
 
     /** Returns a linear fade-out promise for the currently playing track
-     * @remarks The sound is faded to zero.
+     * @remarks The sound is faded to the minimum audio level.
      * An actual fade operation is only started when
      * - the duration is non-zero and
-     * - the current volume is also non-zero
+     * - the current volume is also non-minimum
      * otherwise
      * - a fade with duration zero is started and the promise is immediately resolved.
      * @devdoc Howler only supports linear fade operations
@@ -167,7 +202,7 @@ export default class HowlerFader {
                     console.debug(
                         `HowlerFader::fadeOut:volume:${currentVolume}`,
                     );
-                    if (currentVolume > 0) {
+                    if (currentVolume > this.minAudioLevel) {
                         //Determine the required fade, based on the current volume
                         //(an existing fade-in could be currently running, requiring a partial fade only)
                         const requiredDuration = this.duration * currentVolume;
