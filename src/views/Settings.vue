956b1d814fc56feb1fa6138b215d5a2bc8a14495
marcel <github@marcelsuter.ch>
2022-01-06 23:00:17 +0100

    finished fading settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,6 +2,8 @@
     <div class="container">
         <h1 class="title">Settings</h1>
 
+        <h3 class="subtitle">General</h3>
+
         <div class="field">
             <div class="control">
                 <label class="checkbox">
@@ -32,117 +34,147 @@
             </div>
         </div>
 
-        <hr />
+        <h3 class="subtitle">Audio</h3>
+
+        <div class="field">
+            <div class="control">
+                <label class="label">
+                    Note: The audio settings are applied only at next app start
+                    <span class="has-opacity-half is-size-7">
+                        (If available, in the browser, you can use use the
+                        "Reload" button. Otherwise terminate and re-open the
+                        app.)</span
+                    >
+                </label>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <label class="checkbox">
+                    <input
+                        type="checkbox"
+                        :checked="this.getSettings.useHowlerJsAudioEngine"
+                        @change="useHowlerJsAudioEngineChanged"
+                    />
+                    Use howler.js as audio engine
+                    <span class="has-opacity-half is-size-7">
+                        (supports fading)</span
+                    >
+                </label>
+            </div>
+        </div>
+        <!-- Fading durations, with two columns -->
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <label class="label"
+                        >Fade-in duration
+                        <span class="has-opacity-half is-size-7">
+                            (For play operations)</span
+                        >
+                    </label>
+                    <div class="control">
+                        <div class="select">
+                            <select
+                                v-model.number="
+                                    this.localSettings.fadeInDuration
+                                "
+                                @change="fadeInDurationChanged"
+                                class=""
+                            >
+                                <option v-bind:value="0">no fading</option>
+                                <option v-bind:value="20">
+                                    20 milliseconds
+                                </option>
+                                <option v-bind:value="50">
+                                    50 milliseconds
+                                </option>
+                                <option v-bind:value="100">
+                                    100 milliseconds
+                                </option>
+                                <option v-bind:value="200">
+                                    200 milliseconds
+                                </option>
+                                <option v-bind:value="500">
+                                    500 milliseconds
+                                </option>
+                                <option v-bind:value="1000">1 seconds</option>
+                                <option v-bind:value="2000">2 seconds</option>
+                                <option v-bind:value="5000">5 seconds</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="field">
+                    <label class="label"
+                        >Fade-out duration
+                        <span class="has-opacity-half is-size-7">
+                            (For pause operations)</span
+                        >
+                    </label>
+                    <div class="control">
+                        <div class="select">
+                            <select
+                                v-model.number="
+                                    this.localSettings.fadeOutDuration
+                                "
+                                @change="fadeOutDurationChanged"
+                                class=""
+                            >
+                                <option v-bind:value="0">no fading</option>
+                                <option v-bind:value="20">
+                                    20 milliseconds
+                                </option>
+                                <option v-bind:value="50">
+                                    50 milliseconds
+                                </option>
+                                <option v-bind:value="100">
+                                    100 milliseconds
+                                </option>
+                                <option v-bind:value="200">
+                                    200 milliseconds
+                                </option>
+                                <option v-bind:value="500">
+                                    500 milliseconds
+                                </option>
+                                <option v-bind:value="1000">1 seconds</option>
+                                <option v-bind:value="2000">2 seconds</option>
+                                <option v-bind:value="5000">5 seconds</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <label class="checkbox">
+                    <input
+                        type="checkbox"
+                        :checked="this.getSettings.applyFadeInOffset"
+                        @change="applyFadeInOffsetChanged"
+                    />
+                    Apply an offset before fade-in operations
+                    <span class="has-opacity-half is-size-7">
+                        (The offset compensates for the fade-in duration)</span
+                    >
+                </label>
+            </div>
+        </div>
+
+        <!-- Experimental settings -->
+        <!-- <hr />
 
         <div class="has-text-danger">
             <h1 class="title has-text-danger">Experimental settings</h1>
             <h3 class="subtitle has-text-danger">
                 Here be dragons (use at your own risk)
             </h3>
-
-            <div class="field">
-                <div class="control">
-                    <label class="label has-text-danger">
-                        Note: The audio settings are applied only at next app
-                        start
-                        <span class="has-opacity-half is-size-7">
-                            (If available, in the browser, you can use use the
-                            "Reload" button. Otherwise terminate and re-open the
-                            app.)</span
-                        >
-                    </label>
-                </div>
-            </div>
-
-            <div class="field">
-                <div class="control">
-                    <label class="checkbox">
-                        <input
-                            type="checkbox"
-                            :checked="this.getSettings.useHowlerJsAudioEngine"
-                            @change="useHowlerJsAudioEngineChanged"
-                        />
-                        Use howler.js as audio engine
-                        <span class="has-opacity-half is-size-7">
-                            (may behave differently on playback)</span
-                        >
-                    </label>
-                </div>
-            </div>
-
-            <div class="field">
-                <label class="label has-text-danger"
-                    >Fade-in duration
-                    <span class="has-opacity-half is-size-7">
-                        (For play operations)</span
-                    >
-                </label>
-                <div class="control">
-                    <div class="select has-text-danger">
-                        <select
-                            v-model.number="this.localSettings.fadeInDuration"
-                            @change="fadeInDurationChanged"
-                            class="has-text-danger"
-                        >
-                            <option v-bind:value="0">no fading</option>
-                            <option v-bind:value="20">20 milliseconds</option>
-                            <option v-bind:value="50">50 milliseconds</option>
-                            <option v-bind:value="100">100 milliseconds</option>
-                            <option v-bind:value="200">200 milliseconds</option>
-                            <option v-bind:value="500">500 milliseconds</option>
-                            <option v-bind:value="1000">1 seconds</option>
-                            <option v-bind:value="2000">2 seconds</option>
-                            <option v-bind:value="5000">5 seconds</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-
-            <div class="field">
-                <label class="label has-text-danger"
-                    >Fade-out duration
-                    <span class="has-opacity-half is-size-7">
-                        (For pause operations)</span
-                    >
-                </label>
-                <div class="control">
-                    <div class="select has-text-danger">
-                        <select
-                            v-model.number="this.localSettings.fadeOutDuration"
-                            @change="fadeOutDurationChanged"
-                            class="has-text-danger"
-                        >
-                            <option v-bind:value="0">no fading</option>
-                            <option v-bind:value="20">20 milliseconds</option>
-                            <option v-bind:value="50">50 milliseconds</option>
-                            <option v-bind:value="100">100 milliseconds</option>
-                            <option v-bind:value="200">200 milliseconds</option>
-                            <option v-bind:value="500">500 milliseconds</option>
-                            <option v-bind:value="1000">1 seconds</option>
-                            <option v-bind:value="2000">2 seconds</option>
-                            <option v-bind:value="5000">5 seconds</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-
-            <div class="field">
-                <div class="control">
-                    <label class="checkbox">
-                        <input
-                            type="checkbox"
-                            :checked="this.getSettings.applyFadeInOffset"
-                            @change="applyFadeInOffsetChanged"
-                        />
-                        Apply an offset before fade-in operations
-                        <span class="has-opacity-half is-size-7">
-                            (The offset compensates for the fade-in
-                            duration)</span
-                        >
-                    </label>
-                </div>
-            </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
