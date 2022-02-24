7cbb971dc985d7821f1f454747ab8809ec481178
Marcel Suter <github@marcelsuter.ch>
2022-02-24 07:27:28 +0000

    updated some icons

--- src/components/TrackHeader.vue
+++ src/components/TrackHeader.vue
@@ -30,20 +30,7 @@
                 >
                     <p class="control">
                         <span class="button is-nav">
-                            <span class="icon">
-                                <i class="mdi mdi-24px">
-                                    <!-- Chevron left -->
-                                    <svg
-                                        style="width: 24px; height: 24px"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            fill="currentColor"
-                                            d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"
-                                        />
-                                    </svg>
-                                </i>
-                            </span>
+                            <Icon name="chevron-left" />
                         </span>
                     </p>
                 </router-link>
@@ -127,20 +114,7 @@
                 >
                     <p class="control">
                         <span class="button is-nav">
-                            <span class="icon">
-                                <i class="mdi mdi-24px">
-                                    <!-- Chevron right -->
-                                    <svg
-                                        style="width: 24px; height: 24px"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            fill="currentColor"
-                                            d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"
-                                        />
-                                    </svg>
-                                </i>
-                            </span>
+                            <Icon name="chevron-right" />
                         </span>
                     </p>
                 </router-link>
@@ -155,6 +129,7 @@ import { Track } from '@/store/compilation-types';
 import PlaybackIndicator from '@/components/PlaybackIndicator.vue';
 import CollapsibleButton from '@/components/CollapsibleButton.vue';
 import { ActionTypes } from '@/store/action-types';
+import Icon from '@/components/icons/Icon.vue';
 
 /** Displays a track header with a title.
  * @remarks Also handles the common replayer events for tracks
@@ -165,6 +140,7 @@ export default defineComponent({
     components: {
         CollapsibleButton,
         PlaybackIndicator,
+        Icon,
     },
     props: {
         track: {
