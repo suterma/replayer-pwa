480f75deef94d0f264bb7d3f7cf4803d3eda7bbe
Marcel Suter <github@marcelsuter.ch>
2022-03-08 22:32:23 +0000

    edit as experimental too

--- src/components/NavbarTop.vue
+++ src/components/NavbarTop.vue
@@ -53,12 +53,14 @@ By the click of a button, Replayer starts to play at predefined times in the aud
                     to="/play"
                     >Play</router-link
                 >
-                <router-link
-                    @click="toggleBurger"
-                    class="navbar-item"
-                    to="/edit"
-                    >Edit</router-link
-                >
+                <Experimental>
+                    <router-link
+                        @click="toggleBurger"
+                        class="navbar-item"
+                        to="/edit"
+                        >Edit</router-link
+                    >
+                </Experimental>
                 <Experimental>
                     <router-link
                         @click="toggleBurger"
