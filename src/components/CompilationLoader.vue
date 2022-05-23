4a4c0cbfb8b4ea1ee6698c605fa7acc1fd8ad2c0
suterma <git@marcelsuter.ch>
2022-05-23 23:30:48 +0200

    reworked the persistence into a plugin

--- src/components/CompilationLoader.vue
+++ src/components/CompilationLoader.vue
@@ -1,38 +1,4 @@
-<template>
-    <!-- Query the user in the modal dialog whether to load an existing compilation, if required -->
-    <div :class="{ modal: true, 'is-active': showDialog }">
-        <div class="modal-background"></div>
-
-        <div class="modal-card">
-            <header class="modal-card-head">
-                <h1 class="modal-card-title title">
-                    Retrieve last compilation?
-                </h1>
-            </header>
-            <section class="modal-card-body">
-                <div class="content">
-                    A previously preserved compilation is available. Do you want
-                    to retrieve it or discard it?
-                </div>
-            </section>
-            <footer class="modal-card-foot is-justify-content-flex-end">
-                <button
-                    class="button pl-6 pr-6"
-                    @click="discardLastCompilation()"
-                >
-                    Discard
-                </button>
-
-                <button
-                    class="button is-success pl-6 pr-6"
-                    @click="retrieveLastCompilation()"
-                >
-                    Retrieve
-                </button>
-            </footer>
-        </div>
-    </div>
-</template>
+<template><div></div></template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -41,24 +7,18 @@ import { settingsMixin } from '@/mixins/settingsMixin';
 import CompilationParser from '@/store/compilation-parser';
 import { MutationTypes } from '@/store/mutation-types';
 
-/** A Loader for compilations, from either the URL or the local persistent storage
- * @remarks Provides a dialog for loading the last compilation, if available
+/** A Loader for packages or tracks, from the URL
  * @remarks The order is as follows:
  * 1) if given, load items from the URL parameters (discards any previously persisted compilation)
- * 2) if available, and set to auto-retrieva, automatically retrieve an existing persisted compilation
- * 3) if available, but not set to auto-retrieve, offer it via UI
+ * 2) automatically retrieve an existing persisted compilation
+ * //TODO rework this as something else than a component, maybe into somethign like the persistence plugin
  */
 export default defineComponent({
     name: 'CompilationLoader',
     components: {},
     mixins: [settingsMixin],
-    data() {
-        return {
-            showDialog: false,
-        };
-    },
     mounted: function (): void {
-        //Check whether a given compilation is to be loaded (by Track API or by Auto-Retrieve, if enabled)
+        //Check whether a given compilation is to be loaded (by Track API)
         console.debug('CompilationLoader::mounted:route', this.$route);
         const query = this.$route?.query;
 
@@ -99,41 +59,6 @@ export default defineComponent({
             this.$router.replace({ query: undefined });
             return; //With this package loading
         }
-        //Handle auto-retrieval
-        if (this.getSettings.autoRetrieveLastCompilation) {
-            console.debug(
-                'CompilationLoader::mounted:auto-retriving available compilation',
-            );
-            this.$store.dispatch(ActionTypes.RETRIEVE_COMPILATION);
-            return; //With this compilation retrieving
-        }
-        //Query the user about manual retrieval
-        if (this.hasRetrievableCompilation && !this.hasCompilation) {
-            console.debug('CompilationLoader::mounted:user-choice');
-            this.showDialog = true;
-        }
-    },
-
-    methods: {
-        discardLastCompilation(): void {
-            this.$store.dispatch(ActionTypes.DISCARD_COMPILATION).then(() => {
-                this.showDialog = false;
-            });
-        },
-        retrieveLastCompilation(): void {
-            this.$store.dispatch(ActionTypes.RETRIEVE_COMPILATION).then(() => {
-                this.showDialog = false;
-            });
-        },
-    },
-    computed: {
-        hasRetrievableCompilation(): boolean {
-            return this.$store.getters.hasRetrievableCompilation;
-        },
-        hasCompilation(): boolean {
-            return this.$store.getters.hasCompilation;
-        },
     },
 });
 </script>
-<style scoped></style>
