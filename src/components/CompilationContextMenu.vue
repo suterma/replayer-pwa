345809937cb5779de0888008ee7529ca7f4adfa6
suterma <git@marcelsuter.ch>
2022-07-25 14:03:49 +0200

    removed previous download option

--- src/components/CompilationContextMenu.vue
+++ src/components/CompilationContextMenu.vue
@@ -8,34 +8,16 @@
             }"
             @click="togglePreventScreenTimeoutNow"
         />
-        <Experimental>
-            <Hotkey :keys="['ctrl', 's']" v-slot="{ clickRef }">
-                <DropdownMenuItem
-                    title="Download... [CTRL+S]"
-                    subTitle="Save as file to the device"
-                    @click="download"
-                    :ref="clickRef"
-                />
+        <Hotkey :keys="['ctrl', 's']" v-slot="{ clickRef }">
+            <DropdownMenuItem
+                title="Download... [CTRL+S]"
+                subTitle="Save as file to the device"
+                @click="download"
+                :ref="clickRef"
+            />
 
-                ></Hotkey
-            >
-        </Experimental>
-        <div class="dropdown-item" @click="downloadRezPackage">
-            Download as
-            <span class="has-text-weight-bold">ZIP</span><br />
-            <span class="has-opacity-half is-size-7">
-                (<span class="is-family-monospace">.rez</span>), including media
-                files
-            </span>
-        </div>
-        <div class="dropdown-item" @click="downloadRexFile">
-            Download as
-            <span class="has-text-weight-bold">XML</span><br />
-            <span class="has-opacity-half is-size-7">
-                (<span class="is-family-monospace">.rex</span>), without media
-                files
-            </span>
-        </div>
+            ></Hotkey
+        >
         <hr class="dropdown-divider" />
 
         <DropdownMenuItem
@@ -54,14 +36,13 @@ import { Compilation } from '@/store/compilation-types';
 import DropdownMenu from '@/components/DropdownMenu.vue';
 import DropdownMenuItem from '@/components/DropdownMenuItem.vue';
 import { confirm, downloadCompilation } from '@/code/ui/dialogs';
-import Experimental from './Experimental.vue';
 import { Hotkey } from '@simolation/vue-hotkey';
 
 /** A nav bar as header with a menu for a compilation
  */
 export default defineComponent({
     name: 'CompilationContextMenu',
-    components: { DropdownMenu, DropdownMenuItem, Experimental, Hotkey },
+    components: { DropdownMenu, DropdownMenuItem, Hotkey },
     props: {
         compilation: {
             type: Compilation,
@@ -93,17 +74,6 @@ export default defineComponent({
                     console.debug(`CompilationContextMenu::download done`);
                 }
             });
-        },
-        /** Initiates the download of the current compilation as a single XML (.rex) file
-         */
-        async downloadRexFile(): Promise<void> {
-            this.$store.dispatch(ActionTypes.DOWNLOAD_REX_FILE);
-        },
-
-        /** Initiates the download of the current compilation as a ZIP (.rez) package
-         */
-        async downloadRezPackage(): Promise<void> {
-            this.$store.dispatch(ActionTypes.DOWNLOAD_REZ_PACKAGE);
         },
         togglePreventScreenTimeoutNow() {
             if (this.isPreventingScreenTimeoutNow) {
