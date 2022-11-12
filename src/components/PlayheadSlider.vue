a15ef4da78737052e2c73b67fe5c074205516853
suterma <git@marcelsuter.ch>
2022-11-12 23:28:17 +0100

    simplified playhead slider

--- src/components/PlayheadSlider.vue
+++ src/components/PlayheadSlider.vue
@@ -1,53 +1,51 @@
 <template>
     <!-- Slider without time info around it, as a single control -->
 
-    <template v-if="!showPosition && !showDuration">
-        <input
-            class="slider is-fullwidth is-small is-slim is-circle is-warning"
-            step="stepSize"
-            min="0"
-            :max="track.Duration ?? 0"
-            :value="modelValue"
-            @change="onValueChange"
-            @input="onValueChange"
-            type="range"
-        />
-    </template>
-    <template v-else>
-        <!-- Slider with time info around it -->
-        <div class="level is-mobile is-unselectable">
-            <!-- Left side -->
-            <div class="level-left" v-if="showPosition">
-                <div class="level-item">
-                    <TimeDisplay
-                        class="is-size-7 has-text-warning"
-                        :modelValue="modelValue"
-                    ></TimeDisplay>
-                </div>
-            </div>
-            <div class="level-item mr-0">
-                <input
-                    class="slider is-fullwidth is-small is-slim is-circle is-warning"
-                    step="stepSize"
-                    min="0"
-                    :max="track.Duration ?? 0"
-                    :value="modelValue"
-                    @change="onValueChange"
-                    @input="onValueChange"
-                    type="range"
-                />
-            </div>
-            <!-- Right side -->
-            <div class="level-right" v-if="showDuration">
-                <div class="level-item">
-                    <TimeDisplay
-                        class="has-text-right is-size-7 has-text-warning"
-                        :modelValue="track.Duration"
-                    ></TimeDisplay>
-                </div>
+    <input
+        v-if="!showPosition && !showDuration"
+        class="slider is-fullwidth is-small is-slim is-circle is-warning"
+        step="stepSize"
+        min="0"
+        :max="track.Duration ?? 0"
+        :value="modelValue"
+        @change="onValueChange"
+        @input="onValueChange"
+        type="range"
+    />
+
+    <!-- Slider with time info around it -->
+    <div v-else class="level is-mobile is-unselectable">
+        <!-- Left side -->
+        <div class="level-left" v-if="showPosition">
+            <div class="level-item">
+                <TimeDisplay
+                    class="is-size-7 has-text-warning"
+                    :modelValue="modelValue"
+                ></TimeDisplay>
             </div>
         </div>
-    </template>
+        <div class="level-item mr-0">
+            <input
+                class="slider is-fullwidth is-small is-slim is-circle is-warning"
+                step="stepSize"
+                min="0"
+                :max="track.Duration ?? 0"
+                :value="modelValue"
+                @change="onValueChange"
+                @input="onValueChange"
+                type="range"
+            />
+        </div>
+        <!-- Right side -->
+        <div class="level-right" v-if="showDuration">
+            <div class="level-item">
+                <TimeDisplay
+                    class="has-text-right is-size-7 has-text-warning"
+                    :modelValue="track.Duration"
+                ></TimeDisplay>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
