286566a5c0711c0fde3d92c4acc98876342990ac
suterma <git@marcelsuter.ch>
2023-02-12 23:52:14 +0100

    fixed index for menu

--- src/components/dropdown-menu/DropdownMenu.vue
+++ src/components/dropdown-menu/DropdownMenu.vue
@@ -1,51 +1,71 @@
 <template>
-    <div
-        :class="{
-            dropdown: true,
-            'is-right': true,
-            'is-hoverable': false,
-            'is-active': isDropdownExpanded,
-        }"
-        v-click-outside="collapseDropdown"
+    <!-- 
+    //TODO unfortunately the ESC hotkey does interfere with possibly some other instance
+    //for the ESC hotkey, most probably in the media handlers, possibly also on the dialogs. 
+    -->
+    <Hotkey
+        :enabled="false"
+        :keys="['']"
+        :excluded-elements="[]"
+        @hotkey="collapseDropdown"
     >
-        <!-- dropdown-trigger -->
-        <NavButton
-            aria-haspopup="true"
-            aria-controls="dropdown-menu"
-            :title="title"
-            :iconPath="iconPath"
-            @click="toggleDropdownExpanded()"
-        />
         <div
-            class="dropdown-menu is-unselectable"
-            id="dropdown-menu"
-            role="menu"
-            @click="collapseDropdown()"
+            :class="{
+                dropdown: true,
+                'is-right': true,
+                'is-hoverable': false,
+                'is-active': isDropdownExpanded,
+            }"
+            v-click-outside="collapseDropdown"
         >
-            <div class="dropdown-content" ref="target">
-                <!-- HINT: Because of 'is-static', this should not be clickable, but unfortunately I was not able to prevent this yet -->
-                <div class="dropdown-item is-static is-header has-opacity-half">
-                    <p>{{ title }}</p>
+            <!-- dropdown-trigger -->
+            <!-- z-index must be larger than for h1 (but less then the fixed footer) -->
+            <NavButton
+                style="z-index: 1"
+                aria-haspopup="true"
+                aria-controls="dropdown-menu"
+                :title="title"
+                :iconPath="iconPath"
+                @click="toggleDropdownExpanded()"
+            />
+
+            <!-- z-index must be larger than the fixed footer -->
+            <div
+                style="z-index: 3"
+                class="dropdown-menu is-unselectable"
+                id="dropdown-menu"
+                role="menu"
+                @click="collapseDropdown()"
+            >
+                <div class="dropdown-content" ref="target">
+                    <!-- HINT: Because of 'is-static', this should not be clickable, but unfortunately I was not able to prevent this yet -->
+                    <div
+                        class="dropdown-item is-static is-header has-opacity-half"
+                    >
+                        <p>{{ title }}</p>
+                    </div>
+                    <hr class="dropdown-divider" />
+
+                    <slot>
+                        <!-- The menu items -->
+                    </slot>
                 </div>
-                <hr class="dropdown-divider" />
-                <slot>
-                    <!-- The menu items -->
-                </slot>
             </div>
         </div>
-    </div>
+    </Hotkey>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import NavButton from '@/components/buttons/NavButton.vue';
 import { mdiDotsVertical } from '@mdi/js';
+import { Hotkey } from '@simolation/vue-hotkey';
 
 /** A drop down menu, with a slot for the menu items.
  */
 export default defineComponent({
     name: 'DropdownMenu',
-    components: { NavButton },
+    components: { NavButton, Hotkey },
     props: {
         /* The menu title*/
         title: {
