008d045e5ab652d8e2d3ca412da9169b91e63fca
suterma <git@marcelsuter.ch>
2023-02-16 00:27:56 +0100

    updated comment for the dismissible panel

--- src/components/DismissiblePanel.vue
+++ src/components/DismissiblePanel.vue
@@ -15,7 +15,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { Hotkey } from '@simolation/vue-hotkey';
-/** A panel which can be dismissed either using the ESC key or by clicking outside
+/** A panel which can be dismissed either using the ESC key or by clicking outside of it.
+ * @remarks Does not actually dismiss/hide it's content, but emits a dismissed event instead, which must get handled from the
+ * surrounding component.
+ * @devdoc Registers the "ESC" key as a hotkey and observes clicks outside of the area to emit the dismissed.
  */
 export default defineComponent({
     name: 'DismissiblePanel',
