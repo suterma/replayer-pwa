afc3bee17560e1c8a077f01677a1867df512167a
suterma <git@marcelsuter.ch>
2023-02-27 22:32:13 +0100

    added demo regression test

---
+++ cypress/e2e/regression/69-the-demo-is-not-loading-from-an-external-link.cy.ts
@@ -0,0 +1,12 @@
+/* eslint-disable jest/expect-expect */
+describe('testing the issue "69-the-demo-is-not-loading-from-an-external-link" for regression', () => {
+    it('should load the demo as from the website', () => {
+        // ACT
+        cy.visit('/#/demo');
+
+        // ASSERT that the demo is loaded
+        cy.get('.compilation span.title.is-3').contains(
+            'Demo Compilation (Featuring Lidija Roos)',
+        );
+    });
+});
