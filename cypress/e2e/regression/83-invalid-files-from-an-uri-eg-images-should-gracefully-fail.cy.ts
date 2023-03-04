e00617341bae267f0f0f60c2cd08b0aac61f4c28
suterma <git@marcelsuter.ch>
2023-03-04 15:17:28 +0100

    reorganised test

--- cypress/e2e/regression/83-invalid-files-from-an-uri-eg-images-should-gracefully-fail.cy.ts
+++ cypress/e2e/regression/83-invalid-files-from-an-uri-eg-images-should-gracefully-fail.cy.ts
@@ -1,15 +1,12 @@
 /* eslint-disable jest/expect-expect */
 
 describe('testing the issue "Invalid Files from an URI (e.g. images) should gracefully fail #83" for regression', () => {
-    afterEach(() => {
-        cy.get('[data-cy="notification-danger"]').contains(
-            'Error while retrieving media source',
-        );
-    });
-
     it('should reject a JPEG image', () => {
         cy.loadMediaUrl(
             'https://marcelsuter.ch/wp-content/uploads/2018/04/cropped-IMG_marcelphone_20180323_100356.jpg',
+        );
+        cy.get('[data-cy="notification-danger"]').contains(
+            'Error while retrieving media source',
         );
     });
 
@@ -17,17 +14,26 @@ describe('testing the issue "Invalid Files from an URI (e.g. images) should grac
         cy.loadMediaUrl(
             'https://replayer.app/user/pages/01.home/02._howitworks/visual-function-web-vertical-en.webp',
         );
+        cy.get('[data-cy="notification-danger"]').contains(
+            'Error while retrieving media source',
+        );
     });
 
     it('should reject a PDF', () => {
         cy.loadMediaUrl(
             'https://marcelsuter.ch/wp-content/uploads/2018/08/Complete-compilation-public.pdf',
         );
+        cy.get('[data-cy="notification-danger"]').contains(
+            'Error while retrieving media source',
+        );
     });
 
     it('should reject a Video', () => {
         cy.loadMediaUrl(
             'https://twit.tv/shows/security-now/episodes/901?autostart=false',
+        );
+        cy.get('[data-cy="notification-danger"]').contains(
+            'Error while retrieving media source',
         );
     });
 });
