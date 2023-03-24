66d43417856fd39e4f775da8790f85a2c443d6f2
Marcel Suter <github@marcelsuter.ch>
2023-03-24 21:17:26 +0000

    added a specific reset component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,30 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import {
+    createRouter,
+    createWebHashHistory,
+    isNavigationFailure,
+    NavigationFailureType,
+    RouteRecordRaw,
+} from 'vue-router';
 import Play from '../views/Play.vue';
 import Setlist from '../views/Setlist.vue';
 import Settings from '../views/Settings.vue';
+import Reset from '../views/Reset.vue';
 import About from '../views/About.vue';
 import Demo from '../views/Demo.vue';
 import Development from '../views/Development.vue';
 import { useTitle } from '@vueuse/core';
-import { store } from '@/store/store';
 import { computed } from 'vue';
+import { store } from '@/store/store';
 
 /** The app routes
  * @devdoc route level code-splitting is not used, because it is not supported with ES6/2015
  * Also, the views in Replayer are rather small (especially those rarely used) and would not profit from this anyway.
  */
 const routes: Array<RouteRecordRaw> = [
-    { path: '/', redirect: { name: 'Play' } },
+    {
+        path: '/',
+        redirect: { name: 'Play' },
+    },
     {
         /** The route to the playback
          * @remarks This represents the expectedly most used feature, playback of a file or compilation.
@@ -52,6 +62,11 @@ const routes: Array<RouteRecordRaw> = [
         component: Settings,
     },
     {
+        path: '/reset',
+        name: 'Reset',
+        component: Reset,
+    },
+    {
         path: '/about',
         name: 'About',
         component: About,
@@ -76,7 +91,7 @@ const router = createRouter({
 /** Use the current route and compilation as the title for the document */
 const title = computed(() => {
     let compilationInfo = '';
-    const compilationTitle = store.getters.compilation.Title;
+    const compilationTitle = store.getters.compilation?.Title;
     const toName = router.currentRoute.value.name?.toString();
     //on some routes, also display the compilation title
     if (
@@ -88,8 +103,32 @@ const title = computed(() => {
     }
     return `${toName}${compilationInfo}`;
 });
+
+/**
+ *  Show title according new route
+ */
 useTitle(title, {
     titleTemplate: '%s | Replayer',
+});
+
+/**
+ *  Log changing of the route
+ */
+router.afterEach((to, from, failure) => {
+    if (isNavigationFailure(failure, NavigationFailureType.duplicated)) {
+        // Do not report duplicates
+        return;
+    }
+
+    if (isNavigationFailure(failure)) {
+        console.error(
+            `Route change from ${from.path?.toString()} to ${to.path?.toString()} failed`,
+        );
+    } else {
+        console.info(
+            `Changed route from ${from.path?.toString()} to ${to.path?.toString()}`,
+        );
+    }
 });
 
 export default router;
