81b6421ab0d62ad5656b605782f0b40f002d4dcf
suterma <git@marcelsuter.ch>
2023-05-07 11:30:23 +0200

    used pinia instead of vuex

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,18 +1,13 @@
 import { MediaUrl } from './state-types';
-import { Compilation, ICompilation } from './compilation-types';
+import { Compilation, PlaybackMode } from './compilation-types';
 import CompilationHandler from './compilation-handler';
+import { useLocalStorage } from '@vueuse/core';
 
-/** Defines the state of this application */
-interface IState {
+/** Implements the state of this application */
+export const state = {
     /** A compilation to work with
-     */
-    compilation: ICompilation;
-
-    /** A dictionary of media URLs, representing playable media content
-     * @remarks A name for the resource is used as key, preventing duplicate files for the same content.
-     * For online URL's: a simplified resource name, derived from the URL;
-     * For files: the full name (including a possible path) of the original media file (from the disk or from within a REZ/ZIP-file) */
-    mediaUrls: Map<string, MediaUrl>;
+     *@devdoc An initial, non-null value must be available, otherwise the reactive system does not work */
+    compilation: useLocalStorage('compilation', Compilation.empty()),
 
     /** The currently selected cue Id, if any, otherwise CompilationHandler.EmptyId.
      * This is also used to determine the currently active track.
@@ -22,52 +17,53 @@ interface IState {
      * over a possibly selected track
      * (selectedTrackId should be CompilationHandler.EmptyId in this case anyway.)
      * If no cue is selected, selectedTrackId is used to determine the active track.
-     */
-    selectedCueId: string;
+     * @devdoc An initial, non-null value must be available, otherwise the reactive system does not work */
+    selectedCueId: useLocalStorage('selectedCueId', CompilationHandler.EmptyId),
 
     /** The currently selected track Id, if any, otherwise CompilationHandler.EmptyId.
      * This is also used to determine the currently active track.
      * This serves as alternative when no selected cue Id is set.
      * @remarks This does not control the playback itself. It is intended for display purposes.
      * @remarks Set to CompilationHandler.EmptyId, when no track should be considered selected.
-     */
-    selectedTrackId: string;
+     * @devdoc An initial, non-null value must be available, otherwise the reactive system does not work */
+    selectedTrackId: useLocalStorage(
+        'selectedTrackId',
+        CompilationHandler.EmptyId,
+    ),
+
+    /** A dictionary of media URLs, representing playable media content
+     * @remarks A name for the resource is used as key, preventing duplicate files for the same content.
+     * For online URL's: a simplified resource name, derived from the URL;
+     * For files: the full name (including a possible path) of the original media file (from the disk or from within a REZ/ZIP-file) */
+    mediaUrls: useLocalStorage('mediaUrls', new Map<string, MediaUrl>()),
 
     /** An application work message stack, used for progress indication
      * @remarks during ongoing work, the stack is non-empty
      */
-    progressMessageStack: Array<string>;
-
+    progressMessageStack: useLocalStorage(
+        'progressMessageStack',
+        new Array<string>(),
+    ),
     /** An application error message stack, used for error indication
      * @remarks during unacknowledged errors, the stack is non-empty
      */
-    errorMessageStack: Array<string>;
-
+    errorMessageStack: useLocalStorage(
+        'errorMessageStack',
+        new Array<string>(),
+    ),
+    /** An application success message stack, used for success indication
+     */
+    successMessageStack: useLocalStorage(
+        'successMessageStack',
+        new Array<string>(),
+    ),
     /** Whether to use global app shortcuts
      * @remarks This can be used to temporarily pause global app shortcuts
      * in favor of use within modal or other dialogs.
      */
-    useAppShortcuts: boolean;
-}
-
-export const state: IState = {
-    /** @devdoc An initial, non-null value must be available, otherwise the reactive system does not work */
-    compilation: Compilation.empty(),
-
-    /** @devdoc An initial, non-null value must be available, otherwise the reactive system does not work */
-    selectedCueId: CompilationHandler.EmptyId,
-
-    /** @devdoc An initial, non-null value must be available, otherwise the reactive system does not work */
-
-    selectedTrackId: CompilationHandler.EmptyId,
-
-    mediaUrls: new Map<string, MediaUrl>(),
-
-    progressMessageStack: new Array<string>(),
-
-    errorMessageStack: new Array<string>(),
-
-    useAppShortcuts: true,
+    useAppShortcuts: useLocalStorage('useAppShortcuts', true),
+    /** The playback mode.
+     * @remarks This can be set by the user, and is persisted.
+     */
+    playbackMode: useLocalStorage('playbackMode', PlaybackMode.PlayTrack),
 };
-
-export type State = typeof state;
