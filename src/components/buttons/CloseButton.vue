0db76818d794a63ee80a4d55ad2bd773c21e1e6c
suterma <git@marcelsuter.ch>
2023-05-12 22:43:22 +0200

    added a close button

---
+++ src/components/buttons/CloseButton.vue
@@ -0,0 +1,46 @@
+<template>
+    <NavButton title="close" :iconPath="mdiClose" @click="close()"
+        ><slot></slot
+    ></NavButton>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import NavButton from '@/components/buttons/NavButton.vue';
+import { mdiClose } from '@mdi/js';
+
+/** A button to display and toggle a collapsed or expanded state, using a directed chevron icon.
+ * @remarks The default (expanded) chevron direction is down.
+ */
+export default defineComponent({
+    name: 'CloseButton',
+    emits: ['close'],
+    components: { NavButton },
+    props: {
+        /** Whether this represents the closed state. */
+        modelValue: {
+            type: Boolean,
+            default: false,
+        },
+
+        /* The title*/
+        title: {
+            type: String,
+            default: '',
+        },
+    },
+
+    data() {
+        return {
+            /** Icons from @mdi/js */
+            mdiClose: mdiClose,
+        };
+    },
+
+    methods: {
+        close() {
+            this.$emit('close');
+        },
+    },
+});
+</script>
