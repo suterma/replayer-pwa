0f5cd9ea6ffa797dc70028c57be47c0e4d292d3c
Marcel Suter <github@marcelsuter.ch>
2023-08-26 15:46:12 +0200

    multi-source-testing

--- cypress/e2e/features/update-cue-button-must-set-position.cy.ts
+++ cypress/e2e/features/update-cue-button-must-set-position.cy.ts
@@ -1,45 +1,45 @@
-/* eslint-disable jest/expect-expect */
-describe('the cue update button', () => {
-    it('must set the position according the current playback position (using an audio source)', () => {
-        checkCueUpdateFor(
-            'https://lib.replayer.app/decisions-by-lidija-roos.ogg',
-        );
+const mediaSourceUrls = [
+    {
+        name: 'audio',
+        url: 'https://lib.replayer.app/decisions-by-lidija-roos.ogg',
+    },
+    {
+        name: 'video',
+        url: 'https://lib.replayer.app/nasa-4k/2023_Moon_Phase_North_Up_YouTubeHD.webm',
+    },
+    {
+        name: 'YouTube',
+        url: 'https://www.youtube.com/watch?v=7XzhtWcepos',
+    },
+];
+
+mediaSourceUrls.forEach((mediaSourceUrl) => {
+    describe('the cue update button', () => {
+        it(`must set the position according the current playback position (using an  ${mediaSourceUrl.name} source)`, () => {
+            // ARRANGE (crete a cue at the beginning)
+            cy.visit(`/#/edit?media=${mediaSourceUrl.url}`);
+            cy.get('button[data-cy="insert-cue"]').first().click();
+
+            // ACT (go to the middle of the track)
+            cy.get('button[data-cy="toggle-playback"]').first().click();
+            cy.get('input[type=range]')
+                .as('range')
+                .invoke('val', 50)
+                .trigger('change');
+
+            // ACT (update cue)
+            cy.get('[data-cy="cue-editors"] button[data-cy="adjust-time"]')
+                .first()
+                .click();
+
+            // ASSERT (that the cue was updated.)
+            cy.get(
+                '[data-cy="cue-editors"] input[type=number][data-cy="input-time-position"]',
+            )
+                .invoke('val') // call the val() method to extract the value
+                .then((val) => +(val ?? '')) // convert it to a number
+                .should('be.least', 50); // also compare it to a number
+            //Because it starts to play, it will eventually be greater than the set value}
+        });
     });
-
-    it('must set the position according the current playback position (using a video source)', () => {
-        checkCueUpdateFor(
-            'https://lib.replayer.app/nasa-4k/2023_Moon_Phase_North_Up_YouTubeHD.webm',
-        );
-    });
-
-    it('must set the position according the current playback position (using a YouTube source)', () => {
-        checkCueUpdateFor('https://www.youtube.com/watch?v=7XzhtWcepos');
-    });
-
-    function checkCueUpdateFor(mediaSourceUrl: string): void {
-        // ARRANGE (crete a cue at the beginning)
-        cy.visit(`/#/edit?media=${mediaSourceUrl}`);
-        cy.get('button[data-cy="insert-cue"]').first().click();
-
-        // ACT (go to the middle of the track)
-        cy.get('button[data-cy="toggle-playback"]').first().click();
-        cy.get('input[type=range]')
-            .as('range')
-            .invoke('val', 50)
-            .trigger('change');
-
-        // ACT (update cue)
-        cy.get('[data-cy="cue-editors"] button[data-cy="adjust-time"]')
-            .first()
-            .click();
-
-        // ASSERT (that the cue was updated.)
-        cy.get(
-            '[data-cy="cue-editors"] input[type=number][data-cy="input-time-position"]',
-        )
-            .invoke('val') // call the val() method to extract the value
-            .then((val) => +(val ?? '')) // convert it to a number
-            .should('be.least', 50); // also compare it to a number
-        //Because it starts to play, it will eventually be greater than the set value}
-    }
 });
