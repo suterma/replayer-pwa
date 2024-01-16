dbccb16160edd6a3b3bf7ee7ee17c946a86c1f71
Marcel Suter <github@marcelsuter.ch>
2024-01-17 00:11:24 +0100

    Providing Fullscreen API fallback features

--- src/components/FullscreenPanel.vue
+++ src/components/FullscreenPanel.vue
@@ -2,6 +2,7 @@
     <div ref="container">
         <slot
             :is-fullscreen="isFullscreen"
+            :has-native="hasNative"
             :toggle="toggle"
             :exit="exit"
         ></slot>
@@ -9,19 +10,40 @@
 </template>
 <script setup lang="ts">
 /** A panel for full-screen display using the Fullscreen API, if available.
- * A fallback with a modal dialog is used otherwise.
+ * A fallback is used otherwise.
  * @remarks The parent component can control the slot's fullscreen state
  * via the slot's passed props and functions
  * @devdoc Apple iPhone devices do not support the Fullscreen API.
  * See https://caniuse.com/fullscreen
  */
 import { useFullscreen } from '@vueuse/core';
-import { ref } from 'vue';
-
-/// --- fullscreen ---
+import { onMounted, ref } from 'vue';
 
 const container = ref<HTMLElement | null>(null);
-const { isFullscreen, exit, toggle } = useFullscreen(container);
+
+/// --- assume fullscreen API ---
+const hasNative = ref(true);
+let { isFullscreen, exit, toggle } = useFullscreen(container);
+
+/// --- fallback to emulation if API not available ---
+onMounted(() => {
+    const containerElement = container.value;
+    const hasNativeFullscreen = containerElement?.requestFullscreen;
+    hasNative.value = hasNativeFullscreen != undefined;
+
+    if (!hasNative) {
+        console.log('Providing Fullscreen API fallback features');
+        isFullscreen = ref(false);
+        toggle = () => {
+            isFullscreen.value = !isFullscreen.value;
+            return Promise.resolve();
+        };
+        exit = () => {
+            isFullscreen.value = false;
+            return Promise.resolve();
+        };
+    }
+});
 
 defineExpose({
     /** A parent component can exit fullscreen */
