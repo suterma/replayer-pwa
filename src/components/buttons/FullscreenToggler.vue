df25fe2e0303a28ef4120eac50280c0306a03c43
Marcel Suter <github@marcelsuter.ch>
2024-01-17 22:45:53 +0100

    improved toggle-button

--- src/components/buttons/FullscreenToggler.vue
+++ src/components/buttons/FullscreenToggler.vue
@@ -2,14 +2,14 @@
     <ToggleButton
         class="button"
         :class="{
-            'is-inactive': !model,
+            'is-inactive': !modelValue,
         }"
-        :is-engaged="model"
+        :is-engaged="modelValue"
         engaged-label="fullscreen is on"
         disengaged-label="fullscreen is off"
-        @click="emit('update:modelValue', !model)"
+        @click="emit('update:modelValue', !modelValue)"
     >
-        <BaseIcon v-if="model" :path="mdiFullscreenExit" />
+        <BaseIcon v-if="modelValue" :path="mdiFullscreenExit" />
         <BaseIcon v-else :path="mdiFullscreen" />
     </ToggleButton>
 </template>
@@ -21,7 +21,7 @@ import { mdiFullscreen, mdiFullscreenExit } from '@mdi/js';
 
 /** A toggle button for the fading state
  */
-const model = defineModel({ default: false, required: true });
+const model = defineModel({ required: true });
 
 const emit = defineEmits(['update:modelValue']);
 </script>
