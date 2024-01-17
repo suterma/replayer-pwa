174fbfdcd00b1f8d1ea2c2b5e7c57350937ea41b
Marcel Suter <marcel.suter@nowhow.ch>
2024-01-17 16:38:15 +0100

    updated linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,24 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
-  'extends': [
-    //'plugin:vue/vue3-essential',
-    'plugin:vue/vue3-strongly-recommended',
-    // NOTE: 'plugin:vue/vue3-recommended' includes also 'plugin:vue/vue3-strongly-recommended' and  'plugin:vue/vue3-essential' 
-    //'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    root: true,
+    extends: [
+        //'plugin:vue/vue3-essential',
+        //'plugin:vue/vue3-strongly-recommended',
+        'plugin:vue/vue3-recommended',
+        // NOTE: 'plugin:vue/vue3-recommended' includes also 'plugin:vue/vue3-strongly-recommended' and  'plugin:vue/vue3-essential'
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting',
+    ],
+    overrides: [
+        {
+            files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+            extends: ['plugin:cypress/recommended'],
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+};
