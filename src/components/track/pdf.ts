1276096e28307e5fd0d2d4253cbba443c06a0b2e
Marcel Suter <github@marcelsuter.ch>
2024-05-11 00:25:47 +0200

    fixed pdfjs usage

--- src/components/track/pdf.ts
+++
@@ -1,7 +0,0 @@
-import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf.js';
-import * as pdfWorker from 'pdfjs-dist/legacy/build/pdf.worker.js';
-
-// Setting worker path to worker bundle.
-pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorker;
-
-export { pdfjsLib };
