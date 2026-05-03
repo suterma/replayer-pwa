229de7d04b0baaf37306fe220f8b7350d310bfc7
Marcel <github@marcelsuter.ch>
2026-05-03 17:32:58 +0200

    Updated logger settings with refined log template and added remarks

--- src/composables/LogComposable.ts
+++ src/composables/LogComposable.ts
@@ -2,9 +2,15 @@ import { Logger, type ILogObj, type ISettingsParam } from 'tslog';
 
 let log: Logger<ILogObj> | undefined = undefined;
 
+/** @remarks; A note about these settings:
+ * Having the logger use the console, to have it handle the
+ * log level correctly, does not work using the tslog logger.
+ * To get a clickable source link in the browser's console,
+ * no custom transport can be defined here.*/
 const settings: ISettingsParam<ILogObj> | undefined = {
+    name: 'replayer',
     hideLogPositionForProduction: true,
-    prettyLogTemplate: '{{logLevelName}}\t[{{filePathWithLine}}{{name}}]\t',
+    prettyLogTemplate: '{{hh}}:{{MM}}:{{ss}}.{{ms}} {{logLevelName}}\t',
 };
 
 /** An application global logger instance for Replayer */
