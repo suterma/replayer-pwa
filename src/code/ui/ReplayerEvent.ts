41f693153fb41e94047b36673801386ab4cbb8d5
Marcel <github@marcelsuter.ch>
2026-09-06 14:56:14 +0200

    feat: Added explicit start/stop actions for later use.

--- src/code/ui/ReplayerEvent.ts
+++ src/code/ui/ReplayerEvent.ts
@@ -1,13 +1,17 @@
-/** A set of Replayer events that are emitted by this Keyboard handler */
-
-//TODO move all the handling of these events to app store
-
+/** A set of Replayer events that can be
+ * emitted from Replayer-internal input handlers and
+ * consumed from suitable components. */
 export enum ReplayerEvent {
     BACK_TO_CUE = 'backtocue',
     TO_NEXT_CUE = 'tonextcue',
     TO_PREV_CUE = 'topreviouscue',
     TO_MNEMONIC_CUE = 'tomnemoniccue',
+    /** Starts or stops playback at the current position, depending on the current state. Honors pre-roll and fade operations, if applicable. */
     TOGGLE_PLAYBACK = 'toggleplayback',
+    /** Starts playback at the current position, when paused. Honors pre-roll and fade operations, if applicable. */
+    PLAY = 'play',
+    /** Stops playback at the current position, when playing. Honors pre-roll and fade operations, if applicable. */
+    PAUSE = 'pause',
     /** Rewinds 5 seconds */
     REWIND = 'rewind',
     /** Forwards 5 seconds */
